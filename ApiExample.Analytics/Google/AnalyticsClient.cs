4a9922bb60564522fce3a551d3481b2fa309a63e
Mathew Glodack <glodack@gmail.com>
2014-05-30 17:44:35 -0700

    initial commit

---
+++ ApiExample.Analytics/Google/AnalyticsClient.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiExample.Analytics.Google
+{
+    public class AnalyticsClient
+    {
+        private static string _urlBase = "http://www.google-analytics.com/collect";
+
+        private HttpWebRequest Create()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(_urlBase);
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.Method = "POST";
+
+            String encodedPayloadString = BuildParameterString();
+            Byte[] payLoadBytes = Encoding.UTF8.GetBytes(encodedPayloadString);
+            request.ContentLength = payLoadBytes.Length;
+
+            using (Stream dataStream = request.GetRequestStream())
+            {
+                dataStream.Write(payLoadBytes, 0, payLoadBytes.Length);
+            }
+            return request;
+        }
+        private static void Send(HttpWebRequest request)
+        {
+            try
+            {
+                using (WebResponse response = request.GetResponse()) { }
+            }
+                catch(WebException) { }
+        }
+
+        public void Report()
+        {
+            Send(Create());
+        }
+        public string BuildParameterString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("v=1&");
+            sb.Append("tid=UA-51515444-1&");
+            sb.Append("cid=" + System.Guid.NewGuid().ToString() + "&"); //Figure out how to save this client Id per application, should generate a new one on startup
+            sb.Append("an=ApiExample&");
+            sb.Append("av=0.1&");
+            sb.Append("t=event&");
+            sb.Append("ec=Application&");
+            sb.Append("ea=Startup&");
+            sb.Append("el=Application Started");
+            return sb.ToString();
+        }
+    }
+}
---
+++ ApiExample/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ApiExample.Analytics.Google;
+namespace ApiExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static AnalyticsClient _gaClient;
+        public MainWindow()
+        {
+            _gaClient = new AnalyticsClient();
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _gaClient.Report();
+        }
+    }
+}
